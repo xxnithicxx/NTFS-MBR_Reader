a18df118eef176143c7830e21be9330043f23872
Bui Minh Duc <90423581+bmd1905@users.noreply.github.com>
2023-02-27 10:49:24 +0700

    Renamed done

--- src/SectorReader.java
+++ src/SectorReader.java
@@ -2,11 +2,11 @@ import java.io.DataInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
 
-public class NTFSSectorReader implements AutoCloseable{
+public class SectorReader implements AutoCloseable{
     private final DataInputStream inputStream;
     private final int sectorSize;
 
-    public NTFSSectorReader(FileInputStream fileInputStream, int sectorSize) throws IOException {
+    public SectorReader(FileInputStream fileInputStream, int sectorSize) throws IOException {
         this.inputStream = new DataInputStream(fileInputStream);
         this.sectorSize = sectorSize;
     }
@@ -37,7 +37,7 @@ class Test {
         int sectorSize = 512;
         long sectorNumber = 1234;
 
-        try (NTFSSectorReader reader = new NTFSSectorReader(new FileInputStream(filePath), sectorSize)) {
+        try (SectorReader reader = new SectorReader(new FileInputStream(filePath), sectorSize)) {
             String sectorData = reader.readSector(sectorNumber);
             //System.out.println(sectorData);
             StringBuilder hexBuilder = new StringBuilder();
@@ -49,9 +49,5 @@ class Test {
         } catch (IOException e) {
             System.err.println("Error reading sector: " + e.getMessage());
         }
-
-
-
-
     }
 }
